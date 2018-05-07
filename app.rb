83e0dc628714695c0aaf82afd64d81dd06e4e7c2
Stephen Harrington <stephenharrington04@yahoo.com>
2018-05-07 16:32:45 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  get '/name' do
+    "My name is Steve"
+  end
+
+  get '/hometown' do
+    "My hometown is Albany"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is Thunder"
+  end
+
 end
